c7998938f7ff34c7fe22054e1b9866d36489f876
dina <dinahamadi.20@gmail.com>
2024-08-22 06:26:13 +0100

    update file limits

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,6 +1,7 @@
 # This Puppet manifest updates file descriptor limits for the holberton user to avoid "Too many open files" errors.
 
 exec { 'update-file-limits-for-holberton':
-  command => '/usr/bin/env bash -c "sed -i \'/holberton hard/s/5/50000/\' /etc/security/limits.conf && sed -i \'/holberton soft/s/4/50000/\' /etc/security/limits.conf"',
+  command => '/usr/bin/env bash -c "sed -i \'s/holberton hard nofile [0-9]*/holberton hard nofile 50000/\' /etc/security/limits.conf && sed -i \'s/holberton soft nofile [0-9]*/holberton soft nofile 50000/\' /etc/security/limits.conf"',
   path    => ['/usr/bin', '/bin'],
+  unless  => 'grep -q "holberton hard nofile 50000" /etc/security/limits.conf && grep -q "holberton soft nofile 50000" /etc/security/limits.conf',
 }
