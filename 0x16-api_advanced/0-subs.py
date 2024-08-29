7f10c543fa189d9eabf33ae56da048f9bd36ca26
dina <dinahamadi.20@gmail.com>
2024-08-29 15:55:53 +0100

    sub

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,9 +7,7 @@ import requests
 
 def number_of_subscribers(subreddit):
     """
-    Queries the Reddit API and returns the number of
-    subscribers for a given subreddit.
-    If the subreddit is invalid, returns 0.
+    Queries the Reddit API and returns the number of subscribers or 0.
     """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
