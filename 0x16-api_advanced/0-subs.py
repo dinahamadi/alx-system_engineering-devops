5369bcc71021a5a76286f6a49f8f5dcb2ff9724c
dina <dinahamadi.20@gmail.com>
2024-08-29 15:08:44 +0100

    sub

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -14,15 +14,9 @@ def number_of_subscribers(subreddit):
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'Custom User Agent 1.0'}
 
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
-        if response.status_code == 404:
-            return 0
-        elif response.status_code == 200:
-            data = response.json()
-            return data['data']['subscribers']
-        else:
-            return 0
-    except requests.exceptions.RequestException:
+    if response.status_code == 404:
         return 0
+    data = response.json()
+    return data['data']['subscribers']
