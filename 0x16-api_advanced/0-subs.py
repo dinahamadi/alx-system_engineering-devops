f4af68684869f0731091f1f153960086566db87f
dina <dinahamadi.20@gmail.com>
2024-08-29 17:09:07 +0100

    sub

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""queries the Reddit API and returns the number of subscribers"""
-
+"""Queries the Reddit API and returns the number of subscribers"""
 
 import requests
 
@@ -9,10 +8,16 @@ def number_of_subscribers(subreddit):
     """Queries the Reddit API and returns the number of subscribers or 0."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
-    else:
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            try:
+                data = response.json()
+                subscribers = data['data']['subscribers']
+                return subscribers
+            except (ValueError, KeyError):
+                return 0
+        else:
+            return 0
+    except requests.exceptions.RequestException:
         return 0
