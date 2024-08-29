53430bc8a66413f304c9955b4449d0fd9b72cb29
dina <dinahamadi.20@gmail.com>
2024-08-29 16:17:10 +0100

    response.json

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,4 +11,4 @@ def number_of_subscribers(subreddit):
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code >= 300:
         return 0
-    return response.get("data").get("subscribers")
+    return response.json().get("data").get("subscribers")
