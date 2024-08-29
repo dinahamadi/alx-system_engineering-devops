3d245b6b87f5da8bfe4abf4769facdd23af02ca3
dina <dinahamadi.20@gmail.com>
2024-08-29 22:33:58 +0100

    ignoring the restart

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,3 @@
-# fix_wordpress.pp
 # This Puppet manifest fixes the 'phpp' to 'php' issue in the WordPress wp-settings.php file.
 
 # Execute the sed command to replace 'phpp' with 'php' in the wp-settings.php file
@@ -6,12 +5,4 @@ exec { 'fix-wordpress':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
   path    => ['/usr/local/bin', '/bin'],
   onlyif  => 'grep -q phpp /var/www/html/wp-settings.php',
-  notify  => Exec['restart-apache'],
-}
-
-# Restart Apache if the file is changed
-exec { 'restart-apache':
-  command     => '/usr/sbin/service apache2 restart',
-  path        => ['/usr/local/bin', '/bin'],
-  refreshonly => true,
 }
