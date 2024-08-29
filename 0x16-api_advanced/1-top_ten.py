bbbbd71806bc20fb0f4e7d50d4a53af9ecd49667
dina <dinahamadi.20@gmail.com>
2024-08-29 16:22:20 +0100

    top10

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Queries the Reddit API and prints the titles of the top 10 hot posts for a given subreddit."""
+
+import requests
+
+def top_ten(subreddit):
+    """Queries the Reddit API and prints the titles of the top 10 hot posts for a given subreddit.
+    If the subreddit is invalid, prints None.
+    """
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers={"User-Agent": "My-User-Agent"}
+
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            posts = data.get('data', {}).get('children', [])
+            if posts:
+                for post in posts[:10]:
+                    print(post.get('data', {}).get('title'))
+                return
+        print(None)
+    except requests.exceptions.RequestException:
+        print(None)
