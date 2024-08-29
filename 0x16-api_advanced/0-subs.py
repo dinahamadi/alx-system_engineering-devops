6b93c975b613303381e4200ddde8a28ccb874128
dina <dinahamadi.20@gmail.com>
2024-08-29 15:41:38 +0100

    ignore exception

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -13,13 +13,9 @@ def number_of_subscribers(subreddit):
     """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
-
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 200:
-            data = response.json().get("data")
-            return data["subscribers"]
-        else:
-            return 0
-    except requests.exceptions.RequestException:
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json().get("data")
+        return data["subscribers"]
+    else:
         return 0
