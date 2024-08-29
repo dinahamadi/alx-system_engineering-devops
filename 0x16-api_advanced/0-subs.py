54f27b85270564f18b5485e0513a831022dbed1e
dina <dinahamadi.20@gmail.com>
2024-08-29 15:37:42 +0100

    ignore 404

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,15 +9,19 @@ def number_of_subscribers(subreddit):
     """
     Queries the Reddit API and returns the number of
     subscribers for a given subreddit.
-    If the subreddit is invalid or if there's an error,
-    returns 0.
+    If the subreddit is invalid, returns 0.
     """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json().get("data")
+            return data["subscribers"]
+        else:
+            return 0
+    except requests.exceptions.RequestException:
         return 0
-    data = response.json().get("data")
-    return data.get("subscribers")
